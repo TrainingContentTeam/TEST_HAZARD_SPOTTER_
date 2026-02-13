405fb2526a6978ab2bc7fc2957796bbb1e236136
TrainingContentTeam <jashley@lexipol.com>
2026-02-13 15:29:40 -0600

    Update repository name in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = 'TEST_HAZARD_SPOTTER_';
+const repoName = 'TEST_HAZARD_SPOTTER';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   //base: mode === 'production' ? `/${repoName}/` : '/'
-  base: '/TEST_HAZARD_SPOTTER_/',
+  base: '/TEST_HAZARD_SPOTTER/',
 }));
